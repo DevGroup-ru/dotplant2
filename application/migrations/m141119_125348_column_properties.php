93712619b1ae2b1886f8329ac68a5f8be0d470fb
ivan.salnikov <ivan@skobeeff.ru>
2014-11-19 16:12:52 +0300

    Closes #59 migration for column_stored_properties

---
+++ application/migrations/m141119_125348_column_properties.php
@@ -0,0 +1,27 @@
+<?php
+
+use app\models\Object;
+use yii\db\Schema;
+use yii\db\Migration;
+
+
+class m141119_125348_column_properties extends Migration {
+    public function up() {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        $this->createTable(
+            '{{%category_property}}',
+            [
+                'object_model_id' => 'INT UNSIGNED NOT NULL PRIMARY KEY',
+            ],
+            $tableOptions
+        );
+        $this->update( Object::tableName(), [ 'column_properties_table_name' => 'category_property' ], [ 'id' => 2 ] );
+        $this->update( Object::tableName(), [ 'column_properties_table_name' => 'product_property' ], [ 'id' => 3 ] );
+    }
+
+    public function down() {
+        $this->dropTable( '{{%category_property}}' );
+        $this->update( Object::tableName(), [ 'column_properties_table_name' => 'category_properties' ], [ 'id' => 2 ] );
+        $this->update( Object::tableName(), [ 'column_properties_table_name' => 'product_properties' ], [ 'id' => 3 ] );
+    }
+}
