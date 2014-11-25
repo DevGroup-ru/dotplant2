06c0bee03e08415d932910782858206e760288a4
bethrezen <b37hr3z3n@gmail.com>
2014-11-25 11:46:32 +0300

    Migration fix for data module

---
+++ application/migrations/m141125_084200_data_backend_menu_fix.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141125_084200_data_backend_menu_fix extends Migration
+{
+    public function up()
+    {
+        $this->execute("DELETE FROM {{%backend_menu}} WHERE parent_id = 16 OR id = 16");
+
+        $this->insert(
+            '{{%backend_menu}}',
+            [
+                'parent_id' => '18',
+                'name' => 'Data',
+                'route' => '/data/file/',
+                'icon' => 'database',
+                'sort_order' => '0',
+                'added_by_ext' => 'core',
+                'rbac_check' => 'data manage',
+                'css_class' => '',
+                'translation_category' => 'app'
+            ]
+        );
+
+    }
+
+    public function down()
+    {
+        echo "m141125_084200_data_backend_menu_fix cannot be reverted.\n";
+
+        return false;
+    }
+}
