f686522c0b987685d8aa3c655bea05387ad8acc0
ivan.salnikov <ivan@skobeeff.ru>
2014-12-10 11:17:24 +0300

    full-test environment

--- application/migrations/m141209_151057_subdomain.php
+++ application/migrations/m141209_151057_subdomain.php
@@ -6,26 +6,31 @@ class m141209_151057_subdomain extends Migration
 {
     public function up()
     {
-        $stdIn = fopen("php://stdin", "r");
-        echo "\nEnter server name (ie. localhost): ";
-        $serverName = trim(fgets($stdIn));
-        if (empty($serverName)) {
-            $serverName = 'localhost';
+        $serverName = '';
+        if (getenv("SERVER_NAME")) {
+            $serverName = getenv("SERVER_NAME");
+        } else {
+            $stdIn = fopen("php://stdin", "r");
+            echo "\nEnter server name (ie. localhost): ";
+            $serverName = trim(fgets($stdIn));
+            if (empty($serverName)) {
+                $serverName = 'localhost';
+            }
+            fclose($stdIn);
         }
-        fclose($stdIn);
         $core = \app\models\Config::find()->where(['name' => 'Core'])->one();
-        $this->insert(\app\models\Config::tableName(),[
+        $this->insert(\app\models\Config::tableName(), [
             'parent_id' => $core->id,
             'name' => 'Server name',
             'key' => 'serverName',
             'value' => $serverName,
             'preload' => 1,
-            'path' => $core->path. '.serverName',
+            'path' => $core->path . '.serverName',
         ]);
     }
 
     public function down()
     {
-        $this->delete(\app\models\Config::tableName(),['key'=>'serverName']);
+        $this->delete(\app\models\Config::tableName(), ['key' => 'serverName']);
     }
 }
