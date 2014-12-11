0a07c40f7dbabb00d9b362af7a4d831a6ed9f7d7
bethrezen <b37hr3z3n@gmail.com>
2014-12-11 15:57:07 +0300

    Filters with query params fix

--- application/widgets/filter/js/filter.js
+++ application/widgets/filter/js/filter.js
@@ -5,6 +5,12 @@
 function getUrl(url) {
     var match = url.match(/([^\?]*)/i);
     url = match[1];
+    var query_params_match = url.match(/\?(.*)$/),
+        query_params = "";
+    if (query_params_match !== null) {
+        query_params = '&' + query_params_match[1];
+    }
+
     var params = {};
     $('.filter-dynamic .filter-form [name]').each(function(index) {
         var $this = $(this);
@@ -16,14 +22,16 @@ function getUrl(url) {
     });
     var query = "";
     if (Object.keys(params).length > 0) {
-        query += "?" + $.param(params);
+        query += $.param(params) + query_params;
     }
     return url + query;
 }
 
 jQuery.fn.getFilters = function() {
     $(this).on('click', 'a', function() {
+        console.log(getUrl($(this).attr('href')));
         location.href = getUrl($(this).attr('href'));
+
         return false;
     });
     $(this).find('.filter-form').on('focusout', 'input', function() {
