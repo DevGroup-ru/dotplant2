a6f9ba97d2a44a4e9762fef34e014378eda9ee05
bethrezen <b37hr3z3n@gmail.com>
2014-12-23 17:04:50 +0300

    Don't render empty text properties

--- application/properties/handlers/text/views/frontend-render.php
+++ application/properties/handlers/text/views/frontend-render.php
@@ -16,20 +16,26 @@ use app\models\Property;
 use kartik\helpers\Html;
 
 ?>
-<dl>
+
     <?php
         if (count($values->values) == 0) {
             return;
         }
+    ?>
+
+    <?php
         $property = Property::findById($property_id);
         $result = "";
         foreach ($values->values as $val) {
             if (isset($val['value'])) {
-                $result .= Html::tag('dd', $val['value']);
+                if (!empty(trim($val['value']))) {
+                    $result .= Html::tag('dd', $val['value']);
+                }
             }
         }
+        $result = trim($result);
+
         if (!empty($result)) {
-            echo Html::tag('dt', $property->name) . $result . "\n\n";
+            echo '<dl>' . Html::tag('dt', $property->name) . $result . "</dl>\n\n";
         }
     ?>
-</dl>
