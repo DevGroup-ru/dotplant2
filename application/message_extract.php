b75930d5fcc8f3e9821d4ee96cee3d3b74bd5be1
bethrezen <b37hr3z3n@gmail.com>
2014-12-30 10:41:24 +0300

    @@ fix

--- application/message_extract.php
+++ application/message_extract.php
@@ -17,7 +17,7 @@ return [
     'sort' => true,
     // boolean, whether to remove messages that no longer appear in the source code.
     // Defaults to false, which means each of these messages will be enclosed with a pair of '@@' marks.
-    'removeUnused' => false,
+    'removeUnused' => true,
     // array, list of patterns that specify which files/directories should NOT be processed.
     // If empty or not set, all files/directories will be processed.
     // A path matches a pattern if it contains the pattern string at its end. For example,
