45a600a2d5d1c95501d0910748ad8c861894eeeb
dmitriy <hector.tmb@gmail.com>
2015-01-15 11:12:33 +0300

    revert fix ProductOption Widget

--- application/widgets/options/ProductOption.php
+++ application/widgets/options/ProductOption.php
@@ -57,16 +57,14 @@ class ProductOption extends Widget
                         }
                         $items[$key]['values'][$propValue['psv_id']] = $propValue['name'];
                         $itemsJson[$key] = $propValue['psv_id'];
-
-                        $optionsJson[] = [
-                            'id' => $option->id,
-                            'values' => $itemsJson,
-                            'price' => $option->price,
-                            'old_price' => $option->old_price,
-                        ];
                     }
                 }
-
+                $optionsJson[] = [
+                    'id' => $option->id,
+                    'values' => $itemsJson,
+                    'price' => $option->price,
+                    'old_price' => $option->old_price,
+                ];
             }
             return $this->render(
                 $this->formView,
