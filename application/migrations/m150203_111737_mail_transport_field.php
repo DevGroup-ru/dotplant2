cee8ce6f27408e4b2ba3b8f613fd16d4727a5991
Kirill Djonua <k.djonua@skobeeff.ru>
2015-02-03 14:29:34 +0300

    mail db config migration #58

---
+++ application/migrations/m150203_111737_mail_transport_field.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+use app\models\Config;
+
+class m150203_111737_mail_transport_field extends Migration
+{
+    public function up()
+    {
+        $conf = Config::find()->where(['key' => 'emailConfig'])->one();
+        $this->insert(Config::tableName(),
+            [
+                'parent_id' => $conf->id,
+                'name' => 'Transport',
+                'key' => 'transport',
+                'value' => 'Swift_SmtpTransport'
+            ]
+        );
+    }
+
+    public function down()
+    {
+        $this->delete(Config::tableName(),
+            [
+                'key' => 'transport',
+            ]
+        );
+
+        return true;
+    }
+}
