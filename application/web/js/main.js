cd5d433cbba3d4d6c6d5d2a1666b2693540d9475
fps01 <fps.06@mail.ru>
2015-02-06 14:28:30 +0300

    js cookie methods + .gitignore

--- application/web/js/main.js
+++ application/web/js/main.js
@@ -55,6 +55,18 @@ var DotPlant = {
             'url' : '/user-preferences/set'
         });
         return false;
+    },
+    'setCookie' : function setcookie(name, value, expires, path, domain, secure) {
+        expires instanceof Date ? expires = expires.toGMTString() : typeof(expires) == 'number' && (expires = (new Date(+(new Date) + expires * 1e3)).toGMTString());
+        var r = [name + "=" + escape(value)], s, i;
+        for(i in s = {expires: expires, path: path, domain: domain}){
+            s[i] && r.push(i + "=" + s[i]);
+        }
+        return secure && r.push("secure"), document.cookie = r.join(";"), true;
+    },
+    'getCookie' : function getCookie(name) {
+        var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
     }
 };
 
