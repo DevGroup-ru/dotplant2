d622adf80885e317772b10b9a4e51676c8ee3544
Kirill Djonua <k.djonua@skobeeff.ru>
2015-02-16 15:22:51 +0300

    fix #169 ; deprecated fields was deleted

---
+++ application/migrations/m150216_121527_deleting_append_fields_in_DC.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150216_121527_deleting_append_fields_in_DC extends Migration
+{
+    public function up()
+    {
+        $this->dropColumn('dynamic_content', 'append_content');
+        $this->dropColumn('dynamic_content', 'append_title');
+        $this->dropColumn('dynamic_content', 'append_h1');
+        $this->dropColumn('dynamic_content', 'append_meta_description');
+    }
+
+    public function down()
+    {
+        $this->addColumn('dynamic_content', 'append_content', 'boolean');
+        $this->addColumn('dynamic_content', 'append_title', 'boolean');
+        $this->addColumn('dynamic_content', 'append_h1', 'boolean');
+        $this->addColumn('dynamic_content', 'append_meta_description', 'boolean');
+
+        return true;
+    }
+}
