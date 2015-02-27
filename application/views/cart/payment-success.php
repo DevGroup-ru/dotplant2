a837ca59705055b76a500a36b78506992fbccfa1
Alexander Frolov <thephilosoft@gmail.com>
2015-02-27 22:40:43 +0300

    \s+ => \s in html so, who cares?

--- application/views/cart/payment-success.php
+++ application/views/cart/payment-success.php
@@ -3,11 +3,30 @@
  * @var \yii\web\View $this
  * @var \app\models\Order $order
  */
+
 $this->title = Yii::t('shop', 'Your order has been formed');
 ?>
+
     <p><?= Yii::t('shop', 'Your order has been formed') ?>.</p>
 <?php if (is_null($order)): ?>
-    <p><?= Yii::t('shop', 'You can see your orders') ?> <?= \yii\helpers\Html::a(Yii::t('shop', 'here'), ['/cabinet/orders'], ['class' => 'btn btn-info']) ?></p>
+    <p>
+        <?= Yii::t('shop', 'You can see your orders') ?>
+        <?= \yii\helpers\Html::a(
+            Yii::t('shop', 'here'),
+            ['/cabinet/orders'],
+            ['class' => 'btn btn-info'])
+        ?>
+    </p>
 <?php else: ?>
-    <p><?= Yii::t('shop', 'You can see your order status') ?> <?= \yii\helpers\Html::a(Yii::t('shop', 'here'), ['/cabinet/order', 'id' => $order->hash], ['class' => 'btn btn-info']) ?>.</p>
+    <p>
+        <?= Yii::t('shop', 'You can see your order status') ?>
+        <?= \yii\helpers\Html::a(
+            Yii::t('shop', 'here'),
+            [
+                '/cabinet/order',
+                'id' => $order->hash
+            ],
+            ['class' => 'btn btn-info'])
+        ?>.
+    </p>
 <?php endif; ?>
