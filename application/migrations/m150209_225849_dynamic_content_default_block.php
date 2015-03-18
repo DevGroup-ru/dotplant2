c24fc1f15871141c658e8507dfd1669f7de37b7f
askew- <askew.39rus@gmail.com>
2015-03-18 15:43:03 +0200

    Change dynamic_content on {{%dynamic_content}}

--- application/migrations/m150209_225849_dynamic_content_default_block.php
+++ application/migrations/m150209_225849_dynamic_content_default_block.php
@@ -7,11 +7,11 @@ class m150209_225849_dynamic_content_default_block extends Migration
 {
     public function up()
     {
-        $this->execute("ALTER TABLE `dynamic_content` CHANGE `content_block_name` `content_block_name` VARCHAR(80) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT 'content';");
+        $this->alterColumn('{{%dynamic_content}}', 'content_block_name', 'VARCHAR(80) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT \'content\'');
     }
 
     public function down()
     {
-        $this->execute("ALTER TABLE `dynamic_content` CHANGE `content_block_name` `content_block_name` VARCHAR(80) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT 'bottom-text';");
+        $this->alterColumn('{{%dynamic_content}}', 'content_block_name', 'VARCHAR(80) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT \'bottom-text\'');
     }
 }
