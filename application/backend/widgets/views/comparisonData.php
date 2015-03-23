98ec2a1245f045b5bc77c5cbf2979b0b91bc78b3
dmitriy <hector.tmb@gmail.com>
2015-03-23 14:58:23 +0300

    comparison widget finish

--- application/backend/widgets/views/comparisonData.php
+++ application/backend/widgets/views/comparisonData.php
@@ -38,85 +38,116 @@ use kartik\helpers\Html;
     <?php endforeach; ?>
 </table>
 <script>
+    var comparisonData = {
+        widgetId: '',
+        options : {},
+        data : {},
+        keys: {
+            'field' : 'fields',
+            'property' : 'properties',
+            'relation' : 'relations'
+        },
+        init : function() {
+            self = this;
+            $('#' + this.widgetId + ' .select-list').each(function( index ) {
+                $(this).change(function(){
 
-    var <?=$widgetId?>options = <?= json_encode($options) ?>;
-    var <?=$widgetId?>data = <?= json_encode($data); ?>;
+                    self.displayRow(this);
+                });
+            });
+
+          this.loadData();
+
+        },
+
+        loadData : function() {
+            for (var key in this.data) {
+                element = $('select[name="data['+key+'][type]"]');
+                element
+                    .find("[value='" + this.data[key]['type'] + "']")
+                    .attr('selected', 'selected');
+
+                this.displayRow(element);
+                if (this.data[key]['type'] == 'field' || this.data[key]['type'] == 'property') {
+
+                    $('select[name="data[' + key + '][key]"]')
+                        .find("[value='" + this.data[key]['key'] + "']")
+                        .attr('selected', 'selected');
+                } else if(this.data[key]['type'] == 'relation') {
 
 
+                    $('select[name="data[' + key + '][relationName]"]')
+                        .find("[value='" + this.data[key]['relationName'] + "']")
+                        .attr('selected', 'selected');
+                    $('#' + self.widgetId + ' #data-' + dataKey + ' .relation-options')
+                        .replaceWith(self.createInfoRelation(this.data[key]['type'], key));
+
+                    $('select[name="data[' + key + '][key]"]')
+                        .find("[value='" + this.data[key]['key'] + "']")
+                        .attr('selected', 'selected');
+
+
+                }
+            }
+        },
+        displayRow: function(element) {
+            key = $(':selected', element).val();
+            dataKey = $(element).attr('data-key');
+            if (key == 'field' || key == 'property') {
+                $('#' + this.widgetId + ' #data-' + dataKey + ' td').last().html(this.renderOptions(key, dataKey));
+            } else if(key == 'relation') {
+
+                $('#' + this.widgetId + ' #data-' + dataKey + ' td').last()
+                    .html('<div class="form-inline">')
+                    .append(this.createDropDownRelation(key, dataKey))
+                    .append(this.createInfoRelation(key, dataKey))
+                    .append('</div>');
+
+                self = this;
+                $('#' + this.widgetId + ' #data-' + dataKey + ' [name ="data['+dataKey+'][relationName]"]').change(function(){
+                    $('#' + self.widgetId + ' #data-' + dataKey + ' .relation-options').replaceWith(self.createInfoRelation(key, dataKey));
+                });
+
+            } else {
+                $('#' + this.widgetId + ' #data-' + dataKey + ' td').last().html('');
+            }
+        },
+        renderOptions: function(key, dataKey) {
+            data = this.options[this.keys[key]];
+            select = '<div class="form-group"><select name="data[' + dataKey + '][key]" class="form-control">';
+            for (var k in data) {
+                select += '<option value="' + k + '">' + data[k] + '</option>';
+            }
+            select += '</select></div>';
+            return select;
+        },
+        createDropDownRelation : function( key, dataKey) {
+            data = this.options[this.keys[key]];
+            result = '<div class="form-group "><select name="data[' + dataKey + '][relationName]" class="form-control">';
+            for (var k in data) {
+                result += '<option value="' + k + '">' + data[k]['relationName'] + '</option>';
+            }
+            result += '</select></div>';
+            return result;
+    },
+      createInfoRelation : function( key, dataKey) {
+            selectReletion = $('#' + this.widgetId + ' #data-' + dataKey + ' [name ="data['+ dataKey +'][relationName]"] :selected').val();
+            data = this.options[this.keys[key]][selectReletion]['values'];
+            result = '<div class="form-group relation-options"><select name="data[' + dataKey + '][key]" class="form-control">';
+            for (var k in data) {
+                result += '<option value="' + k + '">' + data[k] + '</option></div>';
+            }
+            result += '</select>'
+
+            result += '<input value="'+ this.options[this.keys[key]][selectReletion]['class'] +'" type="hidden"  name="data[' + dataKey + '][class]" >';
+            return result;
+    }
+    };
     $(function(){
-        for (var key in <?=$widgetId?>data) {
-        $('select[name="data['+key+'][type]"]')
-            .find("[value='" + <?=$widgetId?>data[key]['type'] + "']")
-            .attr('selected', 'selected');
-
-            selectDropDown(
-                <?=$widgetId?>data[key]['type'],
-                key,
-                <?=$widgetId?>options
-            );
-
-            $('select[name="data['+key+'][key]"]')
-                .find("[value='" + <?=$widgetId?>data[key]['key'] + "']")
-                .attr('selected', 'selected');
-        }
-
-
-       $('#<?=$widgetId?> .select-list').change(function(){
-
-           optionValue = $('option:selected', this).attr('value');
-           key = $(this).attr('data-key');
-           selectDropDown(optionValue, key, <?=$widgetId?>options );
-
-       });
+        comparisonData.options = <?= json_encode($options) ?>;
+        comparisonData.data = <?= json_encode($data); ?>;
+        comparisonData.widgetId = '<?=$widgetId?>';
+        comparisonData.init();
     });
-    function selectDropDown(optionValue, key, values) {
-        if ( optionValue == 'field') {
-            $('#data-' + key + ' td').last().html(createDropDown(values.fields, key));
-        } if ( optionValue == 'property') {
-            $('#data-' + key + ' td').last().html(createDropDown(values.properties, key));
-        } if ( optionValue == 'relation') {
-            $('#data-' + key + ' td').last().html('');
-            $('#data-' + key + ' td').last().append(createDropDownRelation(values.relations, key));
-            relationKey =  $('select[name="data[' + key + '][relationName]"] :selected').val();
-            $('#data-' + key + ' td').last().append(createInfoRelation(values.relations[relationKey]['values'], relationKey));
-
-        } if (!optionValue) {
-            $('#data-' + key + ' td').last().html('');
-        }
-    }
-    function createDropDown(data, key) {
-        select = '<select name="data[' + key + '][key]" class="form-control">';
-        for (var k in data) {
-            select += '<option value="' + k + '">' + data[k] + '</option>';
-        }
-        select += '</select>';
-
-        return select;
-    }
-
-
-    function createDropDownRelation(data, key) {
-        result = '<select name="data[' + key + '][relationName]" class="form-control">';
-        for (var k in data) {
-            result += '<option value="' + k + '">' + data[k]['relationName'] + '</option>';
-        }
-        result += '</select>';
-        return result;
-    }
-
-    function createInfoRelation(data, key) {
-
-        console.log(data)
-
-        result = '<select name="data[' + key + '][key]" class="form-control">';
-        for (var k in data) {
-            result += '<option value="' + k + '">' + data[k] + '</option>';
-        }
-        result += '</select>';
-        return result;
-    }
-
-
-
 
 </script>
