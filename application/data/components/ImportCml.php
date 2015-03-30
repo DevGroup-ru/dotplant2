075c06e0ef6e6bf094cbeda8a8b8e73518723404
cyberilya <kola@skobeeff.ru>
2015-03-30 16:27:24 +0300

    format cml

--- application/data/components/ImportCml.php
+++ application/data/components/ImportCml.php
@@ -9,24 +9,24 @@ use Yii;
 class ImportCml extends Import
 {
 
-	public function setData()
-	{
-		$path = Yii::$app->getModule('data')->importDir . '/' . $this->filename;
-		$data = [];
-	
-		if (file_exists($path)) {
-			$xml = new XMLReader ();
-			$xml->open($path);
-			$parser = new CmlGroup2Catalog();
-			$parser->getGroups($xml, 'root');
-			$data = $parser->getData();
-			$xml->close ();
-			unlink($path);
-		}
-	
-		return $data;
-	}
-	public function setData(){
-		return array();
-	}
+    public function setData()
+    {
+        $path = Yii::$app->getModule('data')->importDir . '/' . $this->filename;
+        $data = [];
+    
+        if (file_exists($path)) {
+            $xml = new XMLReader ();
+            $xml->open($path);
+            $parser = new CmlGroup2Catalog();
+            $parser->getGroups($xml, 'root');
+            $data = $parser->getData();
+            $xml->close ();
+            unlink($path);
+        }
+    
+        return $data;
+    }
+    public function setData(){
+        return array();
+    }
 }
