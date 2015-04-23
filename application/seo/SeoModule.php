c9367e0a24d9eef1159d75b66d4e230b2a4cc8ea
Maxim U <zebratuk@gmail.com>
2015-04-23 16:19:28 +0300

    add e-commerce counters for seo

--- application/seo/SeoModule.php
+++ application/seo/SeoModule.php
@@ -3,6 +3,7 @@
 namespace app\seo;
 
 use app\backend\BackendModule;
+use app\seo\controllers\ManageController;
 use app\seo\models\Counter;
 use Yii;
 use yii\base\BootstrapInterface;
@@ -36,5 +37,16 @@ class SeoModule extends BackendModule implements BootstrapInterface
                 $app->getView()->on(View::EVENT_END_BODY, [Counter::className(), 'renderCounters'], $this->include);
             }
         );
+
+        if (null !== $orderId = Yii::$app->session->get('cartOrderId')) {
+            $app->on(
+                Application::EVENT_BEFORE_REQUEST,
+                function () use ($app, $orderId) {
+                    $app->getView()->on(View::EVENT_END_BODY, [ManageController::className(), 'renderEcommerceCounters'], ['orderId' => $orderId]);
+                }
+            );
+
+            Yii::$app->session->remove('cartOrderId');
+        }
     }
 }
