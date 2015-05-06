264f4746989b37bedc2264cba186ddefe9f013ff
Ivan Salnikov <ivan@skobeeff.ru>
2015-05-06 11:43:41 +0300

    fix js add property button in backend/navigation/edit

--- application/backend/views/navigation/navigation-form.php
+++ application/backend/views/navigation/navigation-form.php
@@ -1,14 +1,11 @@
 <?php
-
 use app\backend\widgets\BackendWidget;
 use kartik\helpers\Html;
 use kartik\icons\Icon;
 use kartik\widgets\ActiveForm;
-
 $this->title = Yii::t('app', 'Navigation edit');
 $this->params['breadcrumbs'][] = ['url' => ['/backend/navigation/index'], 'label' => Yii::t('app', 'Navigation')];
 $this->params['breadcrumbs'][] = $this->title;
-
 ?>
 
 <?= app\widgets\Alert::widget([
@@ -18,94 +15,94 @@ $this->params['breadcrumbs'][] = $this->title;
 <?php $form = ActiveForm::begin(['id' => 'navigation-form', 'type'=>ActiveForm::TYPE_HORIZONTAL]); ?>
 
 <?php $this->beginBlock('submit'); ?>
-<div class="form-group no-margin">
-    <?=
-    Html::a(
-        Icon::show('arrow-circle-left') . Yii::t('app', 'Back'),
-        Yii::$app->request->get('returnUrl', ['/backend/navigation/index', 'id' => $model->id]),
-        ['class' => 'btn btn-danger']
-    )
-    ?>
+    <div class="form-group no-margin">
+        <?=
+        Html::a(
+            Icon::show('arrow-circle-left') . Yii::t('app', 'Back'),
+            Yii::$app->request->get('returnUrl', ['/backend/navigation/index', 'id' => $model->id]),
+            ['class' => 'btn btn-danger']
+        )
+        ?>
 
-    <?php if ($model->isNewRecord): ?>
+        <?php if ($model->isNewRecord): ?>
+            <?=
+            Html::submitButton(
+                Icon::show('save') . Yii::t('app', 'Save & Go next'),
+                [
+                    'class' => 'btn btn-success',
+                    'name' => 'action',
+                    'value' => 'next',
+                ]
+            )
+            ?>
+        <?php endif; ?>
+
         <?=
         Html::submitButton(
-            Icon::show('save') . Yii::t('app', 'Save & Go next'),
+            Icon::show('save') . Yii::t('app', 'Save & Go back'),
             [
-                'class' => 'btn btn-success',
+                'class' => 'btn btn-warning',
                 'name' => 'action',
-                'value' => 'next',
+                'value' => 'back',
             ]
         )
         ?>
-    <?php endif; ?>
 
-    <?=
-    Html::submitButton(
-        Icon::show('save') . Yii::t('app', 'Save & Go back'),
-        [
-            'class' => 'btn btn-warning',
-            'name' => 'action',
-            'value' => 'back',
-        ]
-    )
-    ?>
-
-    <?=
-    Html::submitButton(
-        Icon::show('save') . Yii::t('app', 'Save'),
-        [
-            'class' => 'btn btn-primary',
-            'name' => 'action',
-            'value' => 'save',
-        ]
-    )
-    ?>
-</div>
+        <?=
+        Html::submitButton(
+            Icon::show('save') . Yii::t('app', 'Save'),
+            [
+                'class' => 'btn btn-primary',
+                'name' => 'action',
+                'value' => 'save',
+            ]
+        )
+        ?>
+    </div>
 <?php $this->endBlock('submit'); ?>
 
-<section id="widget-grid">
-    <div class="row">
-        <article class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+    <section id="widget-grid">
+        <div class="row">
+            <article class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
 
-            <?php BackendWidget::begin(['title'=> Yii::t('app', 'Navigation item'), 'icon'=>'navicon', 'footer'=>$this->blocks['submit']]); ?>
+                <?php BackendWidget::begin(['title'=> Yii::t('app', 'Navigation item'), 'icon'=>'navicon', 'footer'=>$this->blocks['submit']]); ?>
 
-            <?= $form->field($model, 'name'); ?>
-            <?= $form->field($model, 'advanced_css_class'); ?>
-            <?= $form->field($model, 'sort_order'); ?>
+                <?= $form->field($model, 'name'); ?>
+                <?= $form->field($model, 'advanced_css_class'); ?>
+                <?= $form->field($model, 'sort_order'); ?>
 
-            <?php BackendWidget::end(); ?>
-        </article>
+                <?php BackendWidget::end(); ?>
+            </article>
 
-        <article class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+            <article class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
 
-            <?php BackendWidget::begin(['title'=> Yii::t('app', 'Url'), 'icon'=>'link', 'footer'=>$this->blocks['submit']]); ?>
+                <?php BackendWidget::begin(['title'=> Yii::t('app', 'Url'), 'icon'=>'link', 'footer'=>$this->blocks['submit']]); ?>
 
-            <?= $form->field($model, 'url'); ?>
-            <?= $form->field($model, 'route'); ?>
-            <input type="hidden" name="Navigation[route_params]" id="route_params">
-            <div id="properties">
-                <div class="row">
-                    <div class="col-md-10 col-md-offset-2">
-                        <a href="#" class="btn btn-md btn-primary add-property">
-                            <?= Icon::show('plus') ?>
-                            <?= Yii::t('app', 'Add property') ?>
-                        </a>
-                        <br>
-                        <br>
+                <?= $form->field($model, 'url'); ?>
+                <?= $form->field($model, 'route'); ?>
+                <input type="hidden" name="Navigation[route_params]" id="route_params">
+                <div id="properties">
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-2">
+                            <a href="#" class="btn btn-md btn-primary add-property">
+                                <?= Icon::show('plus') ?>
+                                <?= Yii::t('app', 'Add property') ?>
+                            </a>
+                            <br>
+                            <br>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <?php BackendWidget::end(); ?>
-        </article>
-    </div>
-</section>
+                <?php BackendWidget::end(); ?>
+            </article>
+        </div>
+    </section>
 
 <?php ActiveForm::end(); ?>
 
 
 <script type="x-tmpl-underscore" id="parameter-template">
-    <div id="parameter_<%- index %>" class="form-group row parameter">
+    <div id="parameter_<%- index %>" class="form-group row parameter" style="margin-bottom:15px;">
         <label class="col-md-2 control-label" for="key_<%- index %>">Key</label>
         <div class="col-md-3"><input type="text" id="key_<%- index %>" class="form-control param-key" value=""></div>
         <label class="col-md-1 control-label" for="value_<%- index %>">Value</label>
@@ -118,55 +115,49 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
 </script>
-
-<script>
-    $(function(){
+<?php
+$routeParams = empty($model->route_params) ? "{}" : $model->route_params;
+$this->registerJS("\$(function () {
 
         function addProperty(key, value) {
-            var index = $('.parameter').length;
-            var $property = $(
-                _.template(
-                    $("#parameter-template").html(),
-                    {
-                        index : index
-                    }
-                )
-            );
+            var index = \$('.parameter').length;
+            var compiled = _.template(\$(\"#parameter-template\").html());
 
-            $property.find('#key_'+index).val(key);
-            $property.find('#value_'+index).val(value);
+            var \$property = \$(compiled({'index' : index}));
 
-            $property.find('.btn-remove').click(function(){
-                $(this).parents('#parameter_'+index).remove();
+            \$property.find('#key_' + index).val(key);
+            \$property.find('#value_' + index).val(value);
+
+            \$property.find('.btn-remove').click(function () {
+                $(this).parents('#parameter_' + index).remove();
                 return false;
             });
 
-            $("#properties").append($property);
+            \$(\"#properties\").append(\$property);
         }
 
 
-        $(".add-property").click(function(){
+        \$(\".add-property\").click(function () {
             addProperty('', '');
             return false;
         });
 
-        $("#navigation-form").submit(function(){
+        \$(\"#navigation-form\").submit(function () {
 
             var serialized = {};
-            $(".parameter").each(function(){
-                var key = $(this).find('.param-key').val();
-                var value = $(this).find('.param-val').val();
-                serialized[key]=value;
+            \$(\".parameter\").each(function () {
+                var key = \$(this).find('.param-key').val();
+                var value = \$(this).find('.param-val').val();
+                serialized[key] = value;
             });
 
-            $("#route_params").val(JSON.stringify(serialized));
+            \$(\"#route_params\").val(JSON.stringify(serialized));
 
             return true;
         });
 
-        var current_params = <?= empty($model->route_params) ? "{}" : $model->route_params ?>;
+        var current_params = $routeParams;
         for (var c in current_params) {
-            addProperty(c, current_params[c]);
-        }
-    });
-</script>
+        addProperty(c, current_params[c]);
+    }
+});");
