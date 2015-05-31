90d4166f628131c0a6738a0df2fed8519d6005d3
bethrezen <b37hr3z3n@gmail.com>
2015-05-31 22:45:11 +0300

    Fix plain categories widget

--- application/widgets/views/categories-list.php
+++ application/widgets/views/categories-list.php
@@ -6,7 +6,7 @@ use yii\helpers\Url;
 <ul class="categories-list">
     <?php foreach ($categories as $category): ?>
         <li>
-            <a href="<?=Url::toRoute('/products/list', ['last_category_id'=>$category->id])?>">
+            <a href="<?=Url::to(['/shop/product/list', 'last_category_id'=>$category->id])?>">
                 <?= \kartik\helpers\Html::encode($category->name) ?>
             </a>
         </li>
