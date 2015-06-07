fa68d99eaf62dd8efdc52c9cca9b52ae60b44b01
bethrezen <b37hr3z3n@gmail.com>
2015-06-07 17:32:38 +0300

    servername from env

--- application/modules/installer/commands/InstallController.php
+++ application/modules/installer/commands/InstallController.php
@@ -173,6 +173,9 @@ class InstallController extends Controller
             }
 
         }
+        if (getenv('DP2_SERVER_NAME')) {
+            $model->serverName = getenv('DP2_SERVER_NAME');
+        }
         if (InstallerHelper::writeCommonConfig($model) && InstallerHelper::updateConfigurables()) {
             file_put_contents(Yii::getAlias('@app/installed.mark'), '1');
             $this->stdout("Installation complete!\n", Console::FG_GREEN);
