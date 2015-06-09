4b96211bf79560fd4dc8bedecab47d2ec61a63fa
bethrezen <b37hr3z3n@gmail.com>
2015-06-09 03:35:01 +0300

    assets upd

--- application/web/index.php
+++ application/web/index.php
@@ -3,8 +3,8 @@
 include_once(__DIR__ . '/../modules/seo/redirects/redirects.php');
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_ENV') or define('YII_ENV', 'prod');
 defined('YII_CONSOLE') or define('YII_CONSOLE', false);
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
