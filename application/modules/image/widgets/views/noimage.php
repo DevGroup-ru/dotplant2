fbce267fd2686adfdbdd18a3f74a92868af55a70
dmitriy <hector.tmb@gmail.com>
2015-06-26 16:25:19 +0300

    no image auto css width

--- application/modules/image/widgets/views/noimage.php
+++ application/modules/image/widgets/views/noimage.php
@@ -3,6 +3,6 @@ use yii\helpers\Html;
 
 echo Html::tag(
     'div',
-    Html::img(Yii::$app->getModule('image')->noImageSrc, ['itemprop' => "contentUrl"]),
+    Html::img(Yii::$app->getModule('image')->noImageSrc, ['itemprop' => "contentUrl", 'style'=>'max-width:100%']),
     ['itemscope' => '', 'itemtype' => 'http://schema.org/ImageObject']
 );
