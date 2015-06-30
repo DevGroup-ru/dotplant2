797b3a4cc63c3adfa3410057cca70fa70fe994c5
fps01 <fps.06@mail.ru>
2015-06-30 12:05:11 +0300

    fixed console action `admin/flush-cache`

--- application/backend/actions/FlushCacheConsoleAction.php
+++ application/backend/actions/FlushCacheConsoleAction.php
@@ -1,24 +1,20 @@
 <?php
 
-
 namespace app\backend\actions;
 
 use yii\web\Application;
 use Yii;
 
-class FlushCacheConsoleAction extends FlushCacheAction {
-
-
+class FlushCacheConsoleAction extends FlushCacheAction
+{
     public function run()
     {
-        $config = require(Yii::getAlias('@app/config').'/web.php');
-        $_SERVER['REQUEST_URI'] = '/';
-        Yii::setAlias('@webroot', Yii::getAlias('@app/web'));
+        $config = require(Yii::getAlias('@app/config') . '/web.php');
         $app = new Application($config);
+        Yii::setAlias('@webroot', Yii::getAlias('@app/web'));
         $this->flushCache($app);
         $this->flushAssets();
         echo "ok\n";
         $app->end();
     }
-
 }
