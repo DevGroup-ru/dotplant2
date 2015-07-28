cc48a04209d5ce09c36ed3de7a126fc6fcc01db4
Maxim U <zebratuk@gmail.com>
2015-07-28 11:31:42 +0300

    fix2

--- application/modules/user/models/PasswordResetRequestForm.php
+++ application/modules/user/models/PasswordResetRequestForm.php
@@ -23,7 +23,7 @@ class PasswordResetRequestForm extends Model
             ['email', 'exist',
                 'targetClass' => '\common\models\User',
                 'filter' => ['status' => User::STATUS_ACTIVE],
-                'message' => Yii::t('user', 'There is no user with such email.'),
+                'message' => Yii::t('app', 'There is no user with such email.'),
             ],
         ];
     }
