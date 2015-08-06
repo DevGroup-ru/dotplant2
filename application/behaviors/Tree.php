4da5acc685b175f0c21977eecdf593afd363603f
Ivan Salnikov <ivan@skobeeff.ru>
2015-08-06 13:00:39 +0300

    order by sort_order in Tree behavior

--- application/behaviors/Tree.php
+++ application/behaviors/Tree.php
@@ -18,7 +18,7 @@ class Tree extends Behavior
 {
     public $idAttribute = 'id';
     public $parentIdAttribute = 'parent_id';
-    public $sortOrder = 'id ASC';
+    public $sortOrder = 'sort_order ASC, id ASC';
     public $cascadeDeleting = false; // @todo Set default value equals true and check all models that use Tree behavior
 
     /**
