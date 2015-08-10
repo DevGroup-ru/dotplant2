4601cf992e70041a5166836003cc50b622c9e7c0
Maxim U <zebratuk@gmail.com>
2015-08-10 13:34:40 +0300

    fix review

--- application/modules/review/widgets/views/item.php
+++ application/modules/review/widgets/views/item.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @var bool $allowRate
  * @var \app\models\PropertyGroup[] $groups
@@ -8,31 +7,40 @@
  * @var \app\modules\review\models\Review $model
  * @var \yii\web\View $view
  */
-
 use app\models\Property;
 use kartik\helpers\Html;
 use \kartik\icons\Icon;
 
-if ($model->submission->processed_by_user_id === null) {
-    $userName = Yii::t('app', 'Guest');
-} else {
-    /** @var \app\modules\user\models\User $user */
-    $user = \app\modules\user\models\User::findIdentity($model->submission->processed_by_user_id);
-    $userName = Html::encode($user->getDisplayName());
-}
+/** @var \app\models\Submission $submission */
+$submission = $model->submission;
+
+if (null !== $submission) {
+    if ($submission->processed_by_user_id === null) {
+        $userName = Yii::t('app', 'Guest');
+    } else {
+        /** @var \app\modules\user\models\User $user */
+        $user = \app\modules\user\models\User::findIdentity($submission->processed_by_user_id);
+        $userName = Html::encode($user->getDisplayName());
+    }
 //$model->submission->getPropertyGroups(true);
+} else {
+    $userName = Yii::t('app', 'Guest');
+}
 
 ?>
 <div class="row review">
     <div class="col-md-4">
         <div class="review-date_submitted label label-default">
-            <?= Icon::show('calendar') . date("d.m.Y H:i:s", strtotime($model->submission->date_received)) ?>
+            <?php if (null !== $submission): ?>
+            <?= Icon::show('calendar') . date("d.m.Y H:i:s", strtotime($submission->date_received)) ?>
+            <?php endif; ?>
         </div>
         <div class="review-author">
             <?= Icon::show('user') . $userName ?>
         </div>
     </div>
     <div class="col-md-8"><?= Html::encode($model->review_text) ?>
+    <?php if (null !== $submission): ?>
     <?php foreach ($groups as $group): ?>
         <?php $properties = Property::getForGroupId($group->id); ?>
         <?php foreach ($properties as $property): ?>
@@ -48,5 +56,6 @@ if ($model->submission->processed_by_user_id === null) {
             <?php endif; ?>
         <?php endforeach; ?>
     <?php endforeach; ?>
+    <?php endif; ?>
     </div>
 </div>
