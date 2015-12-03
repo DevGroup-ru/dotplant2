5c4917f3f5bea8304e0ce361f91853b452089259
dmitriy <hector.tmb@gmail.com>
2015-12-03 16:38:58 +0300

    add item_group_id

--- application/modules/shop/components/GoogleMerchants/DefaultHandler.php
+++ application/modules/shop/components/GoogleMerchants/DefaultHandler.php
@@ -4,6 +4,8 @@ namespace app\modules\shop\components\GoogleMerchants;
 
 
 use app\modules\shop\helpers\CurrencyHelper;
+use app\modules\shop\models\Category;
+use app\modules\shop\models\Product;
 use yii\helpers\Url;
 
 
@@ -11,41 +13,71 @@ class DefaultHandler implements ModificationDataInterface
 {
     public static function processData(ModificationDataEvent $event)
     {
-        $url = htmlspecialchars(Url::toRoute(['@product', 'model' => $event->model]));
-        $image = htmlspecialchars($event->model->image->getOriginalUrl());
-        if ($event->model->unlimited_count === 1) {
+        $imageObject = $event->model->image;
+        if (!$imageObject) {
+            return;
+        }
+        $event->data = [
+            'title' => $event->model->name,
+            'description' => $event->model->announce,
+            'link' => $event->sender->host . htmlspecialchars(Url::toRoute(['@product', 'model' => $event->model])),
+            'g:id' => $event->model->id,
+            'g:image_link' => htmlspecialchars(
+                $event->sender->host . $event->model->image->getOriginalUrl()
+            ),
+            'g:condition' => 'new',
+            'g:product_type' => self::getProductType($event->model),
+            'g:availability' => static::getAvailability($event->model),
+            'g:price' => static::getPrice($event->model, $event->sender->mainCurrency)
+        ];
+        if ($event->model->parent_id !== 0) {
+            $event->data['g:item_group_id'] = $event->model->parent_id;
+        }
+    }
+
+
+    protected static function getProductType(Product $model)
+    {
+        $parentIds = $model->getMainCategory()->getParentIds();
+
+        $breadcrumbs = [];
+
+        foreach ($parentIds as $id) {
+            $breadcrumbs[] = Category::find()->select(['name'])->where(['id' => $id])->asArray()->scalar();
+        }
+        $breadcrumbs[] = $model->getMainCategory()->name;
+
+        return htmlspecialchars(implode(' > ', $breadcrumbs));
+
+
+    }
+
+    protected static function getPrice(Product $model, $mainCurrency)
+    {
+        return number_format(
+            CurrencyHelper::convertCurrencies($model->price, $model->currency,
+                $mainCurrency),
+            2,
+            '.',
+            ''
+        ) . ' ' . $mainCurrency->iso_code;
+    }
+
+    protected static function getAvailability(Product $model)
+    {
+        if ($model->unlimited_count === 1) {
             $inStock = 'in stock';
         } else {
             $inStock = 'out of stock';
-            foreach ($event->model->getWarehousesState() as $warehouse) {
+            foreach ($model->getWarehousesState() as $warehouse) {
                 if ($warehouse->in_warehouse > 0) {
                     $inStock = 'in stock';
                     break;
                 }
             }
         }
-
-        $price = number_format(
-                CurrencyHelper::convertCurrencies($event->model->price, $event->model->currency,
-                    $event->sender->mainCurrency),
-                2,
-                '.',
-                ''
-            ) . ' ' . $event->sender->mainCurrency->iso_code;
-
-        $event->data = [
-            'title' => $event->model->name,
-            'description' => $event->model->announce,
-            'link' => $event->sender->host . $url,
-            'g:id' => $event->model->id,
-            'g:image_link' => $image,
-            'g:condition' => 'new',
-            'g:availability' => $inStock,
-            'g:price' => $price
-        ];
-        if ($event->model->parent_id !== 0) {
-            $event->data['g:item_group_id'] = $event->model->parent_id;
-        }
-
+        return $inStock;
     }
+
+
 }
--- application/modules/shop/components/GoogleMerchants/GoogleMerchants.php
+++ application/modules/shop/components/GoogleMerchants/GoogleMerchants.php
@@ -2,102 +2,106 @@
 
 namespace app\modules\shop\components\GoogleMerchants;
 
-use app\modules\shop\helpers\CurrencyHelper;
 use app\modules\shop\models\Currency;
 use app\modules\shop\models\Product;
 use Yii;
+use yii\base\Component;
 use yii\helpers\Html;
-use yii\helpers\Url;
-use yii\helpers\VarDumper;
+
 
 /**
  * Class GoogleMerchants
  * This implementation uses a rss 2.0 template
  * @package app\modules\shop\components\GoogleMerchants
  */
-class GoogleMerchants
+class GoogleMerchants extends Component
 {
-    protected $host = 'https://english-brands.ru';
-    protected $title = 'Site title';
-    protected $description = 'Site description';
-    protected $mainCurrency;
-    protected $fileName = 'feed.xml';
 
-    protected function getHeader()
+    public $host = 'https://localhost';
+    public $title = 'Site title';
+    public $description = 'Site description';
+    public $fileName = 'feed.xml';
+    public $mainCurrency = null;
+    public $handlers = [
+        'app\modules\shop\components\GoogleMerchants\DefaultHandler'
+    ];
+
+
+    protected $data = [];
+
+    const MODIFICATION_DATA = 'modification_data';
+
+    public function init()
     {
-        return <<<HEADER
-<?xml version="1.0"?>
-<rss xmlns:g="http://base.google.com/ns/1.0" version="2.0">
-	<channel>
-		<title>{$this->title}</title>
-		<link>{$this->host}</link>
-		<description>{$this->description}</description>
-
-HEADER;
-    }
-
-    protected function getFooter()
-    {
-        return <<<FOOTER
-	</channel>
-</rss>
-FOOTER;
-
-    }
-
-    /**
-     * @param Product $product
-     * @return string
-     */
-    protected function getItem($product)
-    {
-        $url = htmlspecialchars(Url::toRoute(['@product', 'model' => $product]));
-        $image = htmlspecialchars($product->image->getOriginalUrl());
-        if ($product->unlimited_count === 1) {
-            $inStock = 'in stock';
-        } else {
-            $inStock = 'out of stock';
-            foreach ($product->getWarehousesState() as $warehouse) {
-                if ($warehouse->in_warehouse > 0) {
-                    $inStock = 'in stock';
-                    break;
-                }
+        if ($this->mainCurrency === null) {
+            $this->mainCurrency = Currency::getMainCurrency();
+        }
+        foreach ($this->handlers as $handler) {
+            if (is_subclass_of($handler, ModificationDataInterface::class)) {
+                $this->on(self::MODIFICATION_DATA, [$handler, 'processData']);
             }
         }
-        $price = number_format(
-            CurrencyHelper::convertCurrencies($product->price, $product->currency, $this->mainCurrency),
-            2,
-            '.',
-            ''
-        ) . ' ' . $this->mainCurrency->iso_code;
-        $result = Html::beginTag('item');
-        $result .= Html::tag('title', $product->name)
-            . Html::tag('description', $product->announce)
-            . Html::tag('link', $this->host . $url)
-            . Html::tag('g:id', $product->id)
-            . Html::tag('g:image_link', $image)
-            . Html::tag('g:condition', 'new')
-            . Html::tag('g:availability', $inStock)
-            . Html::tag('g:price', $price);
-        if ($product->parent_id !== 0) {
-            Html::tag('g:item_group_id', $product->parent_id);
+        return parent::init();
+    }
+
+
+    public function saveFeedInFs()
+    {
+        file_put_contents(Yii::getAlias('@webroot/' . $this->fileName), $this->generateFeedByArray($this->getData()));
+    }
+
+    public function getData()
+    {
+        if ($this->data === []) {
+            $event = new ModificationDataEvent();
+            foreach (Product::find()->where(['active' => 1])->limit(2)->all() as $product) {
+                $event->model = $product;
+                $this->trigger(self::MODIFICATION_DATA, $event);
+                $this->data[] = $event->data;
+            }
         }
-        $result .= Html::endTag('item') . "\n";
-        // get product options
-        foreach ($product->children as $child) {
-            $result .= $this->getItem($child);
+        return $this->data;
+    }
+
+    public function generateFeedByArray($data)
+    {
+        $result = "<?xml version=\"1.0\"?><rss xmlns:g=\"http://base.google.com/ns/1.0\" version=\"2.0\">";
+        $result .= $this->generateItem(
+            'channel',
+            $this->generateItem('title', $this->title) .
+            $this->generateItem('link', $this->host) .
+            $this->generateItem('description', $this->description) .
+            $this->generateItems($data)
+        );
+        $result .= "</rss>";
+        return $result;
+    }
+
+    protected function generateItems($data)
+    {
+        $result = "";
+        foreach ($data as $item) {
+            $result .= $this->generateItem('item', $item);
         }
         return $result;
     }
 
-    public function generate()
+    protected function generateItem($tag, $data)
     {
-        $this->mainCurrency = Currency::getMainCurrency();
-        $s = $this->getHeader();
-        foreach (Product::find()->where(['active' => 1])->limit(2)->all() as $product) {
-            $s .= $this->getItem($product);
+        $content = "";
+        if (is_array($data)) {
+            foreach ($data as $key => $value) {
+                $content .= $this->generateItem($key, $value);
+            }
+        } else {
+            $content = $data;
         }
-        $s .= $this->getFooter();
-        file_put_contents(Yii::getAlias('@webroot/' . $this->fileName), $s);
+        return static::tag($tag,$content);
     }
+
+    protected static function tag($name, $content = '', $options = [])
+    {
+        return "<$name" . Html::renderTagAttributes($options) . '>' . $content . "</$name>";
+    }
+
 }
