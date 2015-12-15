4f5140361249bd70a0ee9580fd774ea87c7d7285
dmitriy <hector.tmb@gmail.com>
2015-12-15 13:27:14 +0300

    ace widget

---
+++ application/migrations/m151215_101221_aceWidget.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\db\Migration;
+use yii\helpers\Json;
+
+class m151215_101221_aceWidget extends Migration
+{
+    public function up()
+    {
+
+        $this->insert(
+            '{{%wysiwyg%}}',
+            [
+                'name' => 'Ace',
+                'class_name' => 'devgroup\ace\Ace',
+                'params' => Json::encode([
+                    'mode' => 'html',
+                    'theme' => 'chrome',
+                    'htmlOptions' => [
+                        'width' => '100%',
+                        'height' => '200px'
+                    ]
+                ]),
+                'configuration_model' => null,
+                'configuration_view' => null
+            ]
+        );
+    }
+
+    public function down()
+    {
+       $this->delete(
+           '{{%wysiwyg%}}',
+           [
+               'name' => 'Ace',
+           ]
+       );
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
