7e9bcd67271b8df229e58f4cb445e3bbe1e71d13
fps01 <fps.06@mail.ru>
2015-12-25 11:46:31 +0300

    new version of fxp/composer-asset-plugin

--- application/install.sh
+++ application/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/bin/env php ../composer.phar global require "fxp/composer-asset-plugin:~1.0.0"
+/usr/bin/env php ../composer.phar global require "fxp/composer-asset-plugin:~1.0.3"
 /usr/bin/env php ../composer.phar install --prefer-dist --optimize-autoloader
