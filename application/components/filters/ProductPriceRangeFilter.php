f95cc0fc7b6560faaf1402925251a4d1dbf0ac3f
Ivan Salnikov <ivan@skobeeff.ru>
2016-03-04 11:33:27 +0300

    fix max_fraction_digits bug in PriceRange widget

--- application/components/filters/ProductPriceRangeFilter.php
+++ application/components/filters/ProductPriceRangeFilter.php
@@ -35,7 +35,7 @@ class ProductPriceRangeFilter implements FilterQueryInterface
         if ($min !== floatval($this->minValue)) {
             $cacheKeyAppend .= "[MinPrice:$min]";
             $query = $query->andWhere(
-                Product::tableName() . '.price >=  FLOOR (:min_price * currency.convert_nominal / currency.convert_rate * currency.max_fraction_digits) / currency.max_fraction_digits',
+                Product::tableName() . '.price >=  FLOOR (:min_price * currency.convert_nominal / currency.convert_rate * POW(10, currency.max_fraction_digits)) / POW(10, currency.max_fraction_digits)',
                 [':min_price' => $min]
             )->leftJoin(Currency::tableName() . ' ON currency.id = product.currency_id');
             $get[$this->minAttribute] = $min;
@@ -43,7 +43,7 @@ class ProductPriceRangeFilter implements FilterQueryInterface
         if ($max !== floatval($this->maxValue) && (double) 0 !== floatval($max)) {
             $cacheKeyAppend .= "[MaxPrice:$max]";
             $query = $query->andWhere(
-                Product::tableName() . '.price <= CEILING (:max_price * currency.convert_nominal / currency.convert_rate * currency.max_fraction_digits) / currency.max_fraction_digits',
+                Product::tableName() . '.price <= CEILING (:max_price * currency.convert_nominal / currency.convert_rate * POW(10, currency.max_fraction_digits)) / POW(10, currency.max_fraction_digits)',
                 [':max_price' => $max]
             );
             $get[$this->maxAttribute] = $max;
