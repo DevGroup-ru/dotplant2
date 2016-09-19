bd031e2574b170dd0341aea963e776c1e17c5a18
Alexander Frolov <thephilosoft@gmail.com>
2016-09-19 11:20:24 +0300

    add review modal

--- application/modules/review/widgets/views/reviews.php
+++ application/modules/review/widgets/views/reviews.php
@@ -118,3 +118,20 @@ $allowRate = !empty($ratingGroupName);
         <?php ActiveForm::end(); ?>
     </div>
 </div>
+
+<div class="modal fade" tabindex="-1" role="dialog" id="modal-form-info-review-form">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Отзыв отправлен</h4>
+      </div>
+      <div class="modal-body">
+        <p>\Yii::t('Your review will appear on the website immediately after moderation')</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
